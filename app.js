65a035291526791c795f3bca58e77f0e60042c4b
David Yu <davidyu_2005@hotmail.com>
2017-01-16 21:41:01 -0500

    change id to string

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var fs = require('fs');
 
-var fileName = 'league_items_mobafire_image.json';
+var fileName = 'league_items_category.json';
 
 var json = JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
 json.sort(function(a, b) {
-	a = a.name;
-	b = b.name;
+	a = a.item_name;
+	b = b.item_name;
 
 	return (a < b) ? -1 : (a > b) ? 1 : 0;
 });
 
 for (var i = 0; i < json.length; i++) {
-	json[i].id = i;
+	json[i].id = i.toString();
 }
 
 console.log(json);
